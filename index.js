2540988de600d3f9d5b892c91c373083843c2a1f
Archita <asa410@sfu.ca>
2022-12-18 03:11:18 -0800

    Refactored the changeImage() function to improve understandability

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 function changeImage(image){
     var randomNumber = Math.floor(Math.random()*6)+1;
-    if(randomNumber==1){
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice1.png")
-        console.log("it is one");
-    }
-    else if(randomNumber==2){
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice2.png")
-        console.log("it is two");
-    }
-    else if(randomNumber==3){
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice3.png")
-        console.log("it is three");
-    }
-    else if(randomNumber== 4){
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice4.png")
-        console.log("it is four");
-    }
-    else if(randomNumber==5){
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice5.png")
-        console.log("it is four");
-    }
-    else {
-        document.getElementsByClassName(image)[0].setAttribute("src","images/dice6.png")
-        console.log("it is five");
-    }
+    var images = "images/dice"+randomNumber+".png";
+    document.getElementsByClassName(image)[0].setAttribute("src",images);
     return randomNumber;
     
 }
 
 function changeh1(random1,random2){
     if(random1>random2){
-        document.querySelector("h1").innerHTML="Player 1 Wins!";
+        document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
     }
     else if(random2>random1){
-        document.querySelector("h1").innerHTML="Player 2 Wins!";
+        document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
     }
     else{
         document.querySelector("h1").innerHTML="Draw!";
