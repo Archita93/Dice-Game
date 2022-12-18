acc30115fabe1c54a374a2b0c8ab6732ac60f32c
Archita <asa410@sfu.ca>
2022-12-18 02:00:52 -0800

    Created a function to change the src attribute of both the images

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-console.log(randomNumber1);
+function changeImage(image){
+    var randomNumber = Math.floor(Math.random()*6)+1;
+    if(randomNumber==1){
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice1.png")
+        console.log("it is one");
+    }
+    else if(randomNumber==2){
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice2.png")
+        console.log("it is two");
+    }
+    else if(randomNumber==3){
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice3.png")
+        console.log("it is three");
+    }
+    else if(randomNumber== 4){
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice4.png")
+        console.log("it is four");
+    }
+    else if(randomNumber==5){
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice5.png")
+        console.log("it is four");
+    }
+    else {
+        document.getElementsByClassName(image)[0].setAttribute("src","images/dice6.png")
+        console.log("it is five");
+    }
+    
+}
+
+
+changeImage("image1");
+changeImage("image2");
